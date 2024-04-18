f5506a17ed65334943c228b896b936b0e1b3dd33
Safwoen <113114216+Safwoen@users.noreply.github.com>
2024-04-19 00:09:50 +0100

    help indicators done

---
+++ Assets/Scripts/HelpIndicators.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpIndicators : MonoBehaviour
+{
+    public GameObject Canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            bool isActive = Canvas.activeSelf;
+
+            Canvas.SetActive(!isActive);
+        }
+    }
+
+}
